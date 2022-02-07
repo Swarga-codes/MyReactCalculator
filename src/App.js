af91b4c8060ddb73b051fc81d563be9142436625
Swarga-codes <sd3526@srmist.edu.in>
2022-02-07 22:56:59 +0530

    new commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,63 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from 'react';
+import './index.css';
+
 
 function App() {
+  const [result,setResult]=useState('');
+const btnClick=(event)=>{
+  setResult(result.concat(event.target.value));
+}
+const Calculate=()=>{
+  setResult(eval(result));
+}
+const Wipeout=()=>{
+  setResult('');
+}
+const backSlash=()=>{
+  var str='';
+  var len=result.length;
+  for(var i=0;i<len-1;i++){
+    str+=result.charAt(i);
+  }
+    setResult(str);
+}
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+
+ <div className="calculator">
+ <h1>My Calculator</h1>
+ <div className="fields">
+ <input type="text" placeholder='0' value={result}/> 
+ <div>
+<input type="button" value="AC" id='orange' onClick={Wipeout}/>
+<input type="button" value="←" id='orange' onClick={backSlash}/>
+<input type="button" value="%" id='orange' onClick={btnClick}/>
+<input type="button" value="/" id='orange' onClick={btnClick}/>
+</div> 
+<div>
+<input type="button" value="7" onClick={btnClick}/>
+<input type="button" value="8" onClick={btnClick}/>
+<input type="button" value="9" onClick={btnClick}/>
+<input type="button" value="*" id='orange' onClick={btnClick}/>
+</div>
+<div>
+<input type="button" value="4" onClick={btnClick}/>
+<input type="button" value="5" onClick={btnClick}/>
+<input type="button" value="6" onClick={btnClick}/>
+<input type="button" value="-" id='orange'onClick={btnClick}/>
+</div>
+<div>
+<input type="button" value="1" onClick={btnClick}/>
+<input type="button" value="2" onClick={btnClick}/>
+<input type="button" value="3" onClick={btnClick}/>
+<input type="button" value="+" id='orange' onClick={btnClick}/>
+</div>
+<div>
+<input type="button" value="0" onClick={btnClick}/>
+<input type="button" value="." onClick={btnClick}/>
+<input type="button" value="=" id='equalBtn' onClick={Calculate}/>
+</div>
+</div>
+ </div>
   );
 }
 
